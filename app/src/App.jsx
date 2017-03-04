a557ca89d7e91a1659ba07f7d8a01f755fbe3782
Chris <Chris@Chris-mac.monkeybrains.net>
2017-03-04 02:30:37 -0800

    update structure

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import {render} from 'react-dom';
 
+import TimeContainer from './components/time-container.jsx';
+
 class App extends React.Component {
   render () {
     return (
-      <p>Test</p>
+      <div>
+        <TimeContainer/>
+      </div>
     );
   }
 }
---
+++ app/src/components/time-container.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class TimeContainer extends React.Component {
+  render () {
+    return (
+      <p>test</p>
+    );
+  }
+}
