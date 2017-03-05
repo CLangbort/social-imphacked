edd6f8b83f917555ea8c003412345127cd646f6d
Chris <Chris@Chris-mac.monkeybrains.net>
2017-03-05 13:57:57 -0800

    add photo credits

--- app/src/components/park-info.jsx
+++ app/src/components/park-info.jsx
@@ -6,6 +6,15 @@ export default class SocialMedia extends React.Component {
     return (
       <div className='park-info'>
         {this.props.name}
+        {this.props.credit.web ?(
+          <a href={this.props.credit.web} className='park-info__photo-credits'>
+            { this.props.credit.photographer }
+          </a>
+        ) : (
+          <span className='park-info__photo-credits'>
+            { this.props.credit.photographer }
+          </span>
+        )}
       </div>
     );
   }
