346b0dde82b9cec76f89545c539b3a6d4efe5b0f
Chris <Chris@Chris-mac.local>
2017-03-05 16:28:23 -0800

    clear local storage after 24 hours

--- app/src/actions/requests.js
+++ app/src/actions/requests.js
@@ -1,13 +1,9 @@
 const $ = require ('jquery')
 
 const PIP_DATA = 'peopleInParksData';
+const FULL_DAY = 60 * 60 * 24 * 1000;
 
-export default function() {
-  const peopleInParksData = localStorage.getItem(PIP_DATA);
-
-  if (peopleInParksData) {
-    return Promise.resolve(JSON.parse(peopleInParksData));
-  }
+const _fetchData = function() {
   return $.ajax({
     method: 'GET',
     url: 'https://peopleinparks.herokuapp.com/getDailyInfo',
@@ -17,8 +13,30 @@ export default function() {
       park: request[1].park,
       facts: request[2].facts,
     }
+    record.fetchNewDataTime = new Date().getTime() + FULL_DAY;
 
     localStorage.setItem(PIP_DATA, JSON.stringify(record));
+
     return record;
   });
+}
+
+export default function() {
+  const peopleInParksLocalStorage = localStorage.getItem(PIP_DATA);
+
+  if (peopleInParksLocalStorage) {
+    const peopleInParksData = JSON.parse(peopleInParksLocalStorage);
+    const fetchNewDataTime = peopleInParksData.fetchNewDataTime;
+
+    const shouldFetchDate = fetchNewDataTime - new Date().getTime();
+
+    if (shouldFetchDate < 0) {
+      localStorage.removeItem(PIP_DATA);
+      return _fetchData();
+    }
+
+    return Promise.resolve(peopleInParksData);
+  }
+
+  return _fetchData();
 }
